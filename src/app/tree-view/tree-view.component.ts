6caf7c89c052516a10f1a69ae2ecb2eef4bfd84f
ruslan <yaviustalson@gmail.com>
2019-04-04 15:13:31 +0300

    items sort

--- src/app/tree-view/tree-view.component.ts
+++ src/app/tree-view/tree-view.component.ts
@@ -28,7 +28,15 @@ export class TreeViewComponent implements OnInit{
 	ngOnInit() {
 		const { children, type } = this.data;
 		if (this.data.children) {
-			this.items = children.sort((a) => a.type === 'folder' ? -1 : 1);
+			this.items = children.sort((a, b) => {
+				if (a.type === b.type) {
+					return a.name > b.name ? 1 : -1;
+				} else if (a.type === 'folder') {
+					return -1
+				} else {
+					return 1;
+				}
+			});
 		}
 		this.type = type || 'folder';
 		this.opened = this.initialOpened;
